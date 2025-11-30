3e41907620853ecde8c78a9804419c7cf1c0c141
Kacheri Thahir <thahirkacheri2@gmail.com>
2025-11-30 12:30:48 +0530

    Added Blog App And Blog Models

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls.static import static  #mediafile configuration
+from django.conf import settings            #mediafile configuration
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #mediafile configuration
